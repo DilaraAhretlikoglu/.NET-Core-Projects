3ec05f3ef8e7a27150a23803f026d6e7a58faa87
Dilara Ahretlikoğlu <97093117+DilaraAhretlikoglu@users.noreply.github.com>
2023-08-13 13:05:21 +0300

    Triangle Drawing

---
+++ UcgenCizme/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace UcgenCizme;
+class Triangle
+{
+    public static void TriangleDrawing(int boyut)
+    {
+        for(int i = 0 ; i < boyut ; i++)
+        {
+            for(int j = 0; j<=i ; j++)
+            {
+                Console.Write("*"); //Veriyi yazdırdıktan sonra aynı satırda kalır, yani bir sonraki yazdırmanız aynı satırın sonundan devam eder.
+            }
+            Console.WriteLine(); //Yeni satıra geçmek için
+        }
+
+    }
+    
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Boyut bilgisini girin:");
+        int boyut = int.Parse(Console.ReadLine());
+
+        Triangle.TriangleDrawing(boyut);
+
+
+    }
+}
