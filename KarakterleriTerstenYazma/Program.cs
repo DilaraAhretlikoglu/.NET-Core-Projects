d34e3375e6b84304d23ec0973fcc74d6ffa0b81a
Dilara Ahretlikoğlu <97093117+DilaraAhretlikoglu@users.noreply.github.com>
2023-08-14 14:47:29 +0300

    .

---
+++ KarakterleriTerstenYazma/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace KarakterleriTerstenYazma;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Metni girin:");
+        string metin=Console.ReadLine();
+
+        string yeniMetin = yerDegistir(metin);
+        Console.WriteLine("Degistirilmis metin : " + yeniMetin);
+    }
+    static string yerDegistir(string metin)
+    {
+        char[] karakterDizisi = metin.ToCharArray();
+        for(int i = 1; i < karakterDizisi.Length; i++)
+        {
+            char temp = karakterDizisi[i];
+            karakterDizisi[i]=karakterDizisi[i-1];
+            karakterDizisi[i-1] = temp;
+        }
+        return new string(karakterDizisi);
+    }
+}
